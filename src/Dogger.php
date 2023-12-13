cba8eccda6f1d261e48ade528ae2f277a1d9c9a0
Lucas Boinet <lucas@skyloud.fr>
2023-12-13 09:57:26 +0100

    feat: request dogger api to create issues

--- src/Dogger.php
+++ src/Dogger.php
@@ -1,11 +1,17 @@
 <?php
 namespace Dogger\DoggerSdk;
 
+use Exception;
+
 class Dogger {
 
     private $config;
-
     public function __construct($config) {
+        if ($config->key && $config->url && $config->env) {
+            echo "Please give correct config to dogger instance";
+            return;
+        }
+
         $this->config = $config;
         $this->listenToErrors();
     }
@@ -31,6 +37,42 @@ class Dogger {
     }
 
     private function handleErrorStack($error) {
-        error_log(json_encode($error));
+        $this->send($error);
+
+    }
+
+    private function send($error) {
+        try {
+            $dogger_key = $this->config->key;
+            $dogger_env = $this->config->env;
+
+            $payload = <<<DATA
+            {
+                http_code: 400,
+                message: $error->message,
+                stacktrace: $error->trace,
+                type: error,
+                env: $dogger_env
+            }
+            DATA;
+
+            $headers = array(
+                "Content-Type: application/json",
+                "Authorization: Bearer $dogger_key"
+            );
+
+            $curl = curl_init();
+
+            curl_setopt($curl, CURLOPT_URL, $this->config->url);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
+
+            curl_exec($curl);
+            curl_close($curl);
+        } catch (Exception $e) {
+            echo "Can't send error to dogger please check yourn configuration";
+        }
     }
 }
